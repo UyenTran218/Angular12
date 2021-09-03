f7199aa088af456080c579ed7dbfa9337e8abcd5
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2021-09-04 03:06:47 +0930

    ngFor Application

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,17 @@ export class HomeComponent implements OnInit {
   public name = 'Winnie';
   public age = 15;
   public fruit = ['Apple','Grape', 'Orange', 'Mango', 'Durian'];
+  public fruit2 = [
+    { name: 'Apple', price: 12, discount: false}, 
+    { name: 'Grape', price: 10, discount: true},
+    { name: 'Orange', price: -5, discount: false},
+    { name: 'Mango', price: -10, discount: true}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('fruit = ', this.fruit)
+    console.log('fruit = ', this.fruit2)
   }
 
 }
